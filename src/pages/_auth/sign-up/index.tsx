a77b5601b2fa91d7278e7e6684c68592af966326
Pedro Galembeck <galembeckpedro@gmail.com>
2026-04-25 23:33:56 -0300

    feat: create authentication (registration * sing-in) flow on platform

--- src/pages/_auth/sign-up/index.tsx
+++ src/pages/_auth/sign-up/index.tsx
@@ -3,29 +3,28 @@ import { Navbar } from "@/components/navbar";
 import { SignUpForm } from "./~components/-sign-up-form";
 
 export const Route = createFileRoute("/_auth/sign-up/")({
-	component: SignUpPage,
-	head: () => ({
-		meta: [
-			{ title: "Cadastrar | Recycly - Reciclagem rápida, fácil e eficiente" },
-		],
-	}),
+  component: SignUpPage,
+  head: () => ({
+    meta: [
+      { title: "Cadastrar | Recycly - Reciclagem rápida, fácil e eficiente" },
+    ],
+  }),
 });
 
 function SignUpPage() {
-	return (
-		<main className="flex flex-col bg-[#F7F7F7]">
-			<Navbar />
+  return (
+    <main className="flex flex-col bg-[#F7F7F7]">
+      <Navbar />
 
-			<div className="container mx-auto flex h-screen w-full max-w-6xl flex-1 gap-10 px-4 py-8 md:grid md:grid-cols-2 md:px-6.75 lg:px-8">
-				{/** biome-ignore lint/correctness/useImageSize: not required by @TailwindCSS */}
-				<img
-					alt="Recycling material(s)"
-					className="hidden w-full rounded-[5px] md:flex"
-					src="/assets/images/recycling-materials/material-2.png"
-				/>
+      <div className="container mx-auto flex h-screen w-full max-w-6xl flex-1 gap-10 px-4 py-8 lg:grid lg:grid-cols-2 lg:px-6.75 xl:px-8">
+        <img
+          alt="Recycling material(s)"
+          className="hidden w-full rounded-[5px] md:flex h-full"
+          src="/assets/images/register-cover.png"
+        />
 
-				<SignUpForm />
-			</div>
-		</main>
-	);
+        <SignUpForm />
+      </div>
+    </main>
+  );
 }
